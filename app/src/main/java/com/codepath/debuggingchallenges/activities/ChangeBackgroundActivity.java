bff2778644e27dd9c4d24b2451d4c0183ede1dd4
Omotara Oloye <ooloye@fb.com>
2019-06-27 12:02:29 -0700

    Fixed ChangeBackgroundActivity feature by adding a click listener and deleting a clickable view

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.codepath.debuggingchallenges.R;
 
@@ -15,11 +16,20 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+        final Button go = findViewById(R.id.btnGo);
+        go.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                onGoClick(v);
+            }
+        });
+
     }
 
     public void onGoClick(View view) {
         View mainView = findViewById(android.R.id.content);
         mainView.setBackgroundColor(getNextColor());
+//        Toast.makeText(ChangeBackgroundActivity.this, "Clicked here", Toast.LENGTH_LONG).show();
     }
 
     private int getNextColor() {
@@ -27,4 +37,6 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
         oldColor = newColor;
         return newColor;
     }
+
+
 }
